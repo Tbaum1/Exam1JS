7478821c2ba81daeff2eb63279e87035ac4782f7
ANTHONY TENNENBAUM <anthony.tennenbaum@yahoo.com>
2018-09-17 21:02:44 -0500

    update

--- Exam.js
+++ Exam.js
@@ -4,21 +4,21 @@ const checkOut = {
     value: ["Milk", "Bread", "Cheese", "Ham, Lunch Meat"],
     cost: [3.32,1.29,3.35,4.26],
     printShoppingCart(){
-        return `Name: ${this.firstName} ${this.lastName} <br/>Items Purchased: <br/>`
-    }
-    
+        var sumItems = 0;  
+        document.write(`Name: ${this.firstName} ${this.lastName} <br/>Items Purchased: <br/>`);
+        for (i = 0; i < this.value.length; i++) {            
+            var items = document.write(this.value[i] +  ' ' + '$' + this.cost[i] + "<br/>");
+            //document.write(items + "<br/>");
+            sumItems += this.cost[i];
+            console.log(this.items);
+        };      
+        document.write("Total: $" + sumItems.toFixed(2));   
+        console.log("Total: $" + this.sumItems.toFixed(2));     
+    }    
 }
-
-function printItems(){
-    for (i = 0; i < checkOut.value.length; i++) {            
-        var items = checkOut.value[i] +  ' ' + '$' + checkOut.cost[i];
-        document.write(items + "<br/>");
-        console.log(items);
-    };        
-}
-console.log(checkOut.printShoppingCart());
+console.log(`Name: ${checkOut.firstName} ${checkOut.lastName} Items Purchased: `);
 document.write(checkOut.printShoppingCart());
-document.write(printItems());
+
 
 
 
